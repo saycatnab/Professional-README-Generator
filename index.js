9d5fbf963b7cdb2bbb3e82cf1cfae15fb467d059
saysa <saysaygood83@gmail.com>
2023-10-09 17:09:47 +0100

    Finshed writing in all the questions for the user

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const inquirer = require("inquirer");
 const generateMarkdown = require("./utils/generateMarkdown");
 
 // array of questions for user
+// Questions for the user - this will make the readme file according to their choosing.
 const questions = [
     {
         type: `input`,
@@ -21,6 +22,45 @@ const questions = [
         type: `input`,
         name: `title`,
         message: `What is your projects name? ⋆｡𖦹˖`
+
+    },
+    {
+        type: `input`,
+        name: `description`,
+        message: `Write a short description of your project ⋆｡𖦹˖`
+
+    },
+    {
+        type: `list`,
+        name: `license`,
+        message: `What kind of license would you like? ⋆｡𖦹˖`,
+        choices: ["MIT", "APACHE 2.0", "GPL", "None"]
+
+    },
+    {
+        type: `input`,
+        name: `installation`,
+        message: `What command should be run to install dependencies? ⋆｡𖦹˖`,
+        default: `npm i`
+
+    },
+    {
+        type: `input`,
+        name: `test`,
+        message: `What command should be run to run test? ⋆｡𖦹˖`,
+        default: `npm test`
+
+    },
+    {
+        type: `input`,
+        name: `usage`,
+        message: `What does the user need to know about using the repo? ⋆｡𖦹˖`
+
+    },
+    {
+        type: `input`,
+        name: `contributing`,
+        message: `What does the user need to know about contributing to the repo? ⋆｡𖦹˖`
 
     },
 
